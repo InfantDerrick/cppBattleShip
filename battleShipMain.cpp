757e77265b271cfc46cedb207c85e51f71b6e43d
Infant Derrick Gnanasusairaj <infant.derrick.gnanasusairaj@gmail.com>
2017-10-28 14:47:47 +0530

    Added basic struturing of the playing field

---
+++ battleShipMain.cpp
@@ -0,0 +1,83 @@
+//
+// Created by infan_000 on 10/28/2017.
+//
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+char playArea[10][10];
+int playerShipPosition[5][2];
+char playerShipOrientation[2];
+void updatePlayArea(){
+    system("CLS");
+    for(int i = 0; i < 10; i++){
+        for(int j = 0; j<10; j++){
+            cout<<playArea[i][j]<<" ";
+        }
+        cout<<"\n";
+    }
+}
+void inputPlayerShip(){
+    string orient("");
+    cout<<"Enter ship starting coordinate for ship with 1 value: ";
+    cin>>playerShipPosition[0][0]>>playerShipPosition[0][1];
+    cout<<"Enter ship starting coordinate for ship with 1 value: ";
+    cin>>playerShipPosition[1][0]>>playerShipPosition[1][1];
+    cout<<"Enter ship starting coordinate for ship with 1 value: ";
+    cin>>playerShipPosition[2][0]>>playerShipPosition[2][1];
+    cout<<"Enter ship starting coordinate for ship with 3 value: ";
+    cin>>playerShipPosition[3][0]>>playerShipPosition[3][1];
+    cout<<"Enter orientation: ";
+    cin>>orient;
+    if(orient == "vert"){
+        playerShipOrientation[0] == 'v';
+    }else if (orient == "hor"){
+        playerShipOrientation[0] == 'h';
+    }
+    cout<<"Enter ship starting coordinate for ship with 5 value: ";
+    cin>>playerShipPosition[4][0]>>playerShipPosition[4][1];
+    cout<<"Enter orientation: ";
+    cin>>orient;
+    if(orient == "vert"){
+        playerShipOrientation[1] == 'v';
+    }else if (orient == "hor"){
+        playerShipOrientation[1] == 'h';
+    }
+}
+void setPlayerShip(){
+    playArea[playerShipPosition[0][0] - 1][playerShipPosition[0][1] - 1] = 'l';
+    playArea[playerShipPosition[1][0] - 1][playerShipPosition[1][1] -1] = 'l';
+    playArea[playerShipPosition[2][0] - 1][playerShipPosition[2][1] - 1] = 'l';
+    if(playerShipOrientation[0] == 'v'){
+        playArea[playerShipPosition[3][0] - 1][playerShipPosition[3][1] - 1] = 'l';
+        playArea[playerShipPosition[3][0] - 1][playerShipPosition[3][1]] = 'l';
+        playArea[playerShipPosition[3][0] - 1][playerShipPosition[3][1] + 1] = 'l';
+    }else if(playerShipOrientation[0] = 'h'){
+        playArea[playerShipPosition[3][0] - 1][playerShipPosition[3][1] - 1] = 'l';
+        playArea[playerShipPosition[3][0]][playerShipPosition[3][1] - 1] = 'l';
+        playArea[playerShipPosition[3][0] + 1][playerShipPosition[3][1] - 1] = 'l';
+    }
+    if(playerShipOrientation[0] == 'v'){
+        playArea[playerShipPosition[4][0] - 1][playerShipPosition[4][1] - 1] = 'l';
+        playArea[playerShipPosition[4][0] - 1][playerShipPosition[4][1]] = 'l';
+        playArea[playerShipPosition[4][0] - 1][playerShipPosition[4][1] + 1] = 'l';
+        playArea[playerShipPosition[4][0] - 1][playerShipPosition[4][1] + 2] = 'l';
+        playArea[playerShipPosition[4][0] - 1][playerShipPosition[4][1] + 3] = 'l';
+    }else if(playerShipOrientation[0] = 'h'){
+        playArea[playerShipPosition[4][0] - 1][playerShipPosition[4][1] - 1] = 'l';
+        playArea[playerShipPosition[4][0]][playerShipPosition[4][1] - 1] = 'l';
+        playArea[playerShipPosition[4][0] + 1][playerShipPosition[4][1] - 1] = 'l';
+        playArea[playerShipPosition[4][0] + 2][playerShipPosition[4][1] - 1] = 'l';
+        playArea[playerShipPosition[4][0] + 3][playerShipPosition[4][1] - 1] = 'l';
+    }
+}
+int main(){
+    for(int i = 0; i < 10; i++){
+        for(int j = 0; j<10; j++){
+            playArea[i][j] = 'o';
+        }
+    }
+    inputPlayerShip();
+    setPlayerShip();
+    updatePlayArea();
+}
---
+++ cmake-build-debug/CMakeFiles/cppBattleShip.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cppBattleShip.dir/battleShipMain.cpp.obj"
+  "cppBattleShip.pdb"
+  "cppBattleShip.exe"
+  "cppBattleShip.exe.manifest"
+  "libcppBattleShip.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cppBattleShip.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
